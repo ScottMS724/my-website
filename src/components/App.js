da858ee950cf7eda02497a0df8508818d61c46a8
scottms724 <scottms724@gmail.com>
2021-05-10 09:59:54 -0400

    semantic UI added

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 class App extends React.Component {
     render() {
-      return <h1>App</h1>;
+      return <div className="ui container">
+        <h1>In App component.</h1>
+      </div>
     }
-  }
+  };
 
 export default App

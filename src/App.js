20f866505a747192f1b6bd2f24e6519f1274f3f0
scottms724 <scottms724@gmail.com>
2021-05-10 13:40:47 -0400

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import { Route } from 'react-router-dom'
 
 class App extends React.Component {
     render() {
       return <div className="ui container">
-        <NavBar />
+        <Route path='/' component={NavBar} />
       </div>
     }
   };

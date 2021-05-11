e09eb83fab16e1646d6cd2d7fb273313ded472c5
scottms724 <scottms724@gmail.com>
2021-05-11 11:23:44 -0400

    header CSS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,23 +3,26 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <div className="ui secondary pointing menu">
-            <div className="right menu">
-                <Link to='/home' className="item">
-                    Home
+        <div className="ui clearing segment ui inverted segment">
+            <div className="ui right floated header">
+                <Link to='/home' className="item font20">
+                    Home<span> &nbsp; </span>
                 </Link>
 
-                <Link to='/about' className="item">
-                    About
+                <Link to='/about' className="item font20">
+                    About<span> &nbsp; </span>
                 </Link>
 
-                <Link to='/projects' className="item">
-                    Projects
+                <Link to='/projects' className="item font20">
+                    Projects<span> &nbsp; </span>
                 </Link>
                 
-                <Link to='/contact' className="item">
-                    Contact 
+                <Link to='/contact' className="item font20">
+                    Contact
                 </Link>
+            </div>
+            <div className="ui left floated header ui orange header">
+                <h1>Scott Silverman</h1>
             </div>
         </div>
     );

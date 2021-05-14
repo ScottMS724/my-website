56cbfb37fae0d7cb451e36115469231ff5e5f3ee
scottms724 <scottms724@gmail.com>
2021-05-14 16:18:46 -0400

    typo

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <p>As a graduate of Flatiron School's software engineering course, I have significant experience
             building full stack web apps with a React/Redux frontend and Ruby on Rails API backend. My goal when writing code is to
             prioritize readability and adaptability through the use of established best practices and conventions. I love finding simple
-            solutions to common problems through code. I have a B.S. in Biology and
+            solutions to common problems through code. I have a B.S. in biology and
             previous personal training experience, which I believe gives me a unique background and set of soft skills 
             to apply in the tech world.</p>
 

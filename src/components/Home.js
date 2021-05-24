4a96eea4d24e5524713709645aedafdbc5d3eab5
scottms724 <scottms724@gmail.com>
2021-05-24 13:35:56 -0400

    modify home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
                     <img className="headshotsize" src={Headshot} alt=""/>
                 </div>
                 <div className="column">
-                    <p className="fontsize30">Hi, my name is Scott Silverman. I'm a full stack web developer from New York. Welcome to my website. 
-                    I am currently searching for a full stack web developer position.
+                    <p className="fontsize30">Hi, my name is Scott Silverman. I'm a full stack software developer from New York. Welcome to my website. 
+                    I am currently searching for a web developer position.
                     Please click the links above to learn more about me, some recent projects I have programmed, and how to contact.</p>
                 </div>
             </div>

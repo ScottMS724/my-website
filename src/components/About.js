3cb2de5daaaaae5bba7bbf76be37f558d3bf61d6
scottms724 <scottms724@gmail.com>
2022-02-08 11:38:21 -0500

    added updated resume and experience with PatientStudio

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,9 @@ const About = () => {
             <h1>About Me</h1>
 
             <p className="fontsize20">As a graduate of Flatiron School's software engineering course, I have significant experience
-            building full stack web apps with a React/Redux frontend and Ruby on Rails API backend, and using Git, SQL, RESTful API conventions, Bootstrap, 
-            and Semantic UI. My goal when writing code is to prioritize readability and adaptability through the use of established 
+            building full stack web apps with a React/Redux frontend and Ruby on Rails API backend, and using Git, SQL, RESTful API conventions, Tailwind, 
+            and Bootstrap. My current internship with PatientStudio has given me the experience of working TypeScript, MobX and GraphQL. It also exposed me to common tech practices such as SCRUM and important web apps like Jira.
+            My goal when writing code is to prioritize readability and adaptability through the use of established 
             best practices and conventions. I enjoy finding simple solutions to common problems through code. 
             I have a B.S. in biology from Boston University and previous experience working as a professional, certified (NSCA) personal trainer and physical therapists' aide. 
             This gives me a unique background in client-facing jobs where I developed strong interpersonal skills. Combined with my technical skills honed with Flatiron, I
@@ -16,7 +17,7 @@ const About = () => {
             <p className="fontsize20">I know that software engineering is an ever-evolving field that is constantly in flux. An open, inquisive mind 
             willing to learn new things is the key to success, and I embrace the challenge.</p>
 
-            <p className="fontsize20">Prospective employers, please check out my <a href="https://drive.google.com/file/d/1cjKvh2Iieq1BOCWsnA0TA1n_XF5AYbba/view?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a>, as well as my <a href="https://rails-react-blog-24.herokuapp.com/" target="_blank" rel="noopener noreferrer">programming blog</a>.</p>
+            <p className="fontsize20">Prospective employers, please check out my <a href="https://docs.google.com/document/d/1yO5t4-rxPCg8XO-PVA-rIhxwkCIeInK622n6yrdYPEA/edit?usp=sharing" target="_blank" rel="noopener noreferrer">resume</a>, as well as my <a href="https://rails-react-blog-24.herokuapp.com/" target="_blank" rel="noopener noreferrer">programming blog</a>.</p>
 
             <p className="fontsize20">When I'm not programming, my hobbies and interests include PC gaming, weight-lifting, biology, and being a podcast junkie. I'm also a
             big Mets fan. <i class="baseball ball icon"></i></p> 
